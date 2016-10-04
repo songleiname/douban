f0e1e2a38f1fd5608b9298d1d1feddb8c462c522
song lei <435811453@qq.com>
2016-10-04 12:24:29 +0800

    豆瓣FM

---
+++ js/douban.js
@@ -0,0 +1,35 @@
+(function(){
+	var audio = document.getElementById("bgMusic")
+	var paused = document.getElementById("paused")
+	var firsttwo = document.getElementById("firsttwo")
+	var tanchu = document.getElementById("firstthird")
+	var jixv = document.getElementById("jixv")
+	
+//	audio.autoplay = true;
+	paused.onclick = function(){
+		audio.pause();
+		tanchu.style.display = "block"
+		jixv.style.display = "block"
+		paused.style.display = "none"
+	}
+	
+	jixv.onclick = function(){
+		audio.play();
+		tanchu.style.display = "none"
+		jixv.style.display = "none"
+		paused.style.display = "block"
+	}
+	/*//播放(继续播放)
+	
+	 
+	//暂停
+	audio.pause();
+	 
+	//停止
+	audio.pause();
+	audio.currentTime = 0;
+	 
+	//重新播放
+	audio.currentTime = 0;
+	audio.play();*/
+}(window))
