c5c42c28b14d6d85200f5f37c18df331f79bee51
song lei <435811453@qq.com>
2016-10-06 21:23:55 +0800

    豆瓣播放器

---
+++ js/bianse.js
@@ -0,0 +1,123 @@
+(function(){
+	document.getElementById('gdone').onmouseover = function(){
+		this.style.background = '#9DD6C5'
+	}
+    document.getElementById('gdone').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('gdtwo').onmouseover = function(){
+    	this.style.background = '#9DD6C5'
+    }
+    document.getElementById('gdtwo').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('gdthrid').onmouseover = function(){
+    	this.style.background = '#9DD6C5'
+    }
+    document.getElementById('gdthrid').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('gdfour').onmouseover = function(){
+    	this.style.background = '#9DD6C5'
+    }
+    document.getElementById('gdfour').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('gdfive').onmouseover = function(){
+    	this.style.background = '#9DD6C5'
+    }
+    document.getElementById('gdfive').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('gdsix').onmouseover = function(){
+    	this.style.background = '#9DD6C5'
+    }
+    document.getElementById('gdsix').onmouseout = function(){
+    	this.style.background = ''
+    }
+    
+    document.getElementById('wodeone').onmouseover = function(){
+    	this.style.background = '#BCCAC5'
+    }
+    document.getElementById('wodeone').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('wodetwo').onmouseover = function(){
+    	this.style.background = '#BCCAC5'
+    }
+    document.getElementById('wodetwo').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('doubanjing').onmouseover = function(){
+    	this.style.background = '#BCCAC5'
+    }
+    document.getElementById('doubanjing').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('bulu').onmouseover = function(){
+    	this.style.background = '#BCCAC5'
+    }
+    document.getElementById('bulu').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('lading').onmouseover = function(){
+    	this.style.background = '#BCCAC5'
+    }
+    document.getElementById('lading').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('minyao').onmouseover = function(){
+    	this.style.background = '#BCCAC5'
+    }
+    document.getElementById('minyao').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('yaogun').onmouseover = function(){
+    	this.style.background = '#BCCAC5'
+    }
+    document.getElementById('yaogun').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('liuxing').onmouseover = function(){
+    	this.style.background = '#BCCAC5'
+    }
+    document.getElementById('liuxing').onmouseout = function(){
+    	this.style.background = ''
+    }
+    document.getElementById('zuichu').onmouseover = function(){
+    	this.style.background = '#BCCAC5'
+    }
+    document.getElementById('zuichu').onmouseout = function(){
+    	this.style.background = ''
+    }
+    
+    document.getElementById('tingxiangsi').onmouseover = function(){
+    	this.style.background = '#9DD6C5'
+    }
+    document.getElementById('tingxiangsi').onmouseout = function(){
+    	this.style.background = ''
+    }
+    
+    /*document.getElementById('imgxin').onmouseover = function(){
+    	document.getElementById('imgxin').style.display = "none"
+    	document.getElementById('imgxinb').style.display = "block"
+    }
+    document.getElementById('imgxin').onmouseout = function(){
+    	document.getElementById('imgxinb').style.display = "none"
+    	document.getElementById('imgxin').style.display = "block"
+    }
+    document.getElementById('imglaj').onmouseover = function(){
+    	document.getElementById('imglaj').style.display = "none"
+    	document.getElementById('imglajb').style.display = "block"
+    }
+    document.getElementById('imglaj').onmouseout = function(){
+    	document.getElementById('imglajb').style.display = "none"
+    	document.getElementById('imglaj').style.display = "block"
+    }
+    document.getElementById('imgnext').onmouseover = function(){
+    	this.src = "img/2016-10-05_185709.jpg"
+    }
+    document.getElementById('imgnext').onmouseout = function(){
+    	this.src = "img/06.jpg"
+    }*/
+}(window))
--- js/douban.js
+++ js/douban.js
@@ -6,9 +6,35 @@
 	var jixv = document.getElementById("jixv")
 	var dianji = document.getElementById("dianji");
 	var zuocetanchu = document.getElementById("zuocetanchu");
-	var dianjituihui = document.getElementById("dianjituihui")
+	var dianjituihui = document.getElementById("dianjituihui")	
+	
+	var denglubeijing = document.getElementById("denglubeijing")
+	var denglu = document.getElementById("denglu")
+	var zhuce = document.getElementById("dengluer")
+	var guanbione = document.getElementById("guanbione")
+	var guanbizhuce = document.getElementById("guanbizhuce")	
+	var denglujinru = document.getElementById("denglujinru");
+	var mashangzhuce =document.getElementById("mashangzhuce")
+	
+	guanbione.onclick = function(){
+		denglu.style.display = "none"
+		denglubeijing.style.display = "none";
+	}
+	guanbizhuce.onclick = function (){
+		zhuce.style.display = "none";
+		denglubeijing.style.display = "none";
+	}
+	denglujinru.onclick = function(){
+		denglu.style.display = "block";
+		denglubeijing.style.display = "block"
+	}
+	mashangzhuce.onclick = function(){
+		zhuce.style.display = "block";
+		denglubeijing.style.display = "block"
+	}
 	
 	audio.autoplay = true;
+	
 	paused.onclick = function(){
 		audio.pause();
 		tanchu.style.display = "block"
@@ -31,6 +57,144 @@
 	dianjituihui.onclick = function(){
 		zuocetanchu.style.display = "none";
 		dianji.style.display = "block"
+	}
+	
+	setInterval(function(){
+		var gdimg = document.getElementById("gundongimg")
+		if (gdimg.src.match("gd1.jpg")) {
+			gdimg.src = "img/gd2.jpg"
+			beijingtwo.style.display = "block"
+		} else if(gdimg.src.match("gd2.jpg")){
+			gdimg.src = "img/gd3.jpg"
+			beijingthrid.style.display = "block"
+		}else if(gdimg.src.match("gd3.jpg")){
+			gdimg.src = "img/gd4.jpg"
+			beijingfour.style.display = "block"
+		}else if(gdimg.src.match("gd4.jpg")){
+			gdimg.src = "img/gd5.jpg"
+			beijingfive.style.display = "block"
+		}else if(gdimg.src.match("gd5.jpg")){
+			gdimg.src = "img/gd6.jpg"
+			beijingsix.style.display = "block"
+		}else if(gdimg.src.match("gd6.jpg")){
+			gdimg.src = "img/gd1.jpg"
+			beijingtwo.style.display = "none"
+			beijingthrid.style.display = "none"
+			beijingfour.style.display = "none"
+			beijingfive.style.display = "none"
+			beijingsix.style.display = "none"
+			beijing.style.display = "block"
+		}
+	},2000);
+	
+	var gdone =document.getElementById("gdone");
+	gdone.onmousemove = function(){
+		document.getElementById("gundongimg").src="img/gd1.jpg"
+		beijing.style.display = "block"
+		beijingtwo.style.display = "none"
+		beijingthrid.style.display = "none"
+		beijingfour.style.display = "none"
+		beijingfive.style.display = "none"
+		beijingsix.style.display = "none"
+	}			
+	var gdtwo =document.getElementById("gdtwo");
+	gdtwo.onmousemove = function(){
+		document.getElementById("gundongimg").src="img/gd2.jpg"
+		beijing.style.display = "none"
+		beijingtwo.style.display = "block"
+		beijingthrid.style.display = "none"
+		beijingfour.style.display = "none"
+		beijingfive.style.display = "none"
+		beijingsix.style.display = "none"
+	}
+	var gdthrid =document.getElementById("gdthrid");
+	gdthrid.onmousemove = function(){
+		document.getElementById("gundongimg").src="img/gd3.jpg"
+		beijing.style.display = "none"
+		beijingtwo.style.display = "none"
+		beijingthrid.style.display = "block"
+		beijingfour.style.display = "none"
+		beijingfive.style.display = "none"
+		beijingsix.style.display = "none"
+	}
+	var gdfour =document.getElementById("gdfour");
+	gdfour.onmousemove = function(){
+		document.getElementById("gundongimg").src="img/gd4.jpg"
+		beijing.style.display = "none"
+		beijingtwo.style.display = "none"
+		beijingthrid.style.display = "none"
+		beijingfour.style.display = "block"
+		beijingfive.style.display = "none"
+		beijingsix.style.display = "none"
+	}
+	var gdfive =document.getElementById("gdfive");
+	gdfive.onmousemove = function(){
+		document.getElementById("gundongimg").src="img/gd5.jpg"
+		beijing.style.display = "none"
+		beijingtwo.style.display = "none"
+		beijingthrid.style.display = "none"
+		beijingfour.style.display = "none"
+		beijingfive.style.display = "block"
+		beijingsix.style.display = "none"
+	}
+	var gdsix =document.getElementById("gdsix");
+	gdsix.onmousemove = function(){
+		document.getElementById("gundongimg").src="img/gd6.jpg"
+		beijingsix.style.display = "block"
+		beijingtwo.style.display = "none"
+		beijingthrid.style.display = "none"
+		beijingfour.style.display = "none"
+		beijingfive.style.display = "none"
+		beijing.style.display = "none"
+	}
+	
+	var fenxiangzheshou = document.getElementById("fenxiangzheshou");
+	var	fenxiangtwo = document.getElementById("fenxiangtwo")
+	fenxiangzheshou.onmouseover = function(){		
+		fenxiangzheshou.style.display = "none"
+		fenxiangtwo.style.display = "block"
+	}
+	fenxiangzheshou.onmouseout = function(){
+		fenxiangzheshou.style.display = "block"
+		fenxiangtwo.style.display = "none"
+	}
+	
+	var wodeone = document.getElementById("wodeone");
+	wodeone.onclick = function(){
+		wodeone.style.backgroundColor = "red"
+	}
+	
+	audio.ontimeupdate = function(){
+		var sj = audio.currentTime;
+		var miao = (parseInt(sj))%60
+		var fen = (parseInt(sj/60))%60
+		if(miao<10){
+			miao = '0' + miao;
+		}
+		if(fen<10){
+			fen = '0' + fen;
+		}		
+		document.getElementById("shijian").innerHTML= fen +":"+ miao + "/" + parseInt(audio.duration/60)+":"+parseInt(audio.duration%60);	
+		var percent = audio.currentTime / audio.duration;
+		var waitiao = document.getElementById("waitiao");
+    	var neitiao = document.getElementById("neitiao");
+    	neitiao.style.width = percent * (waitiao.offsetWidth)  + "px";
+	}
+	var waitiao = document.getElementById("waitiao");
+    var neitiao = document.getElementById("neitiao");
+	/*waitiao.onclick = function(event){
+		neitiao.style.width = event.offsetX+ "px";
+		audio.currentTime = parseInt(event.offsetX)/225;
+	}*/
+	var waitiao = document.getElementById("waitiao");
+    var neitiao = document.getElementById("neitiao");
+	waitiao.onmousedown = function(){
+		waitiao.onmousemove = function(){
+			audio.currentTime = parseInt(event.offsetX)/225*audio.duration;
+		}
+		waitiao.onmouseup = function(){
+			waitiao.onmousemove = null;
+		}
 	}
 	/*//播放(继续播放)
 	
